fdc3ffc12be6496fae04e0abec48532a1b4f4c98
Hoang Ty <tuonghoangty123@gmail.com>
2020-03-10 08:37:23 +0700

    Update

--- src/main/java/controller/RootController.java
+++ src/main/java/controller/RootController.java
@@ -1,8 +1,17 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
+	@GetMapping("/user")
+	public String getName() 
+	{
+		return "HelloWord";
+		
+	}
+	
+	
 
 }
